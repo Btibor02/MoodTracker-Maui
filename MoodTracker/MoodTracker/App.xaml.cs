7da5a72d6b9a4670aa0152cf432083b9d0b7d73a
Tibor Blascsok <tiborblascsok02@gmail.com>
2024-06-07 05:16:53 +0200

    WelcomeScreen GUI

---
+++ MoodTracker/MoodTracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MoodTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
